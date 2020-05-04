5dc8a3c24316d491b0cff88aca3308b8f2d7780e
Andrei Muja <andrei.raiden20@gmail.com>
2020-05-04 23:07:42 +0300

    Updates

---
+++ precache-manifest.d779c6610b36ee830ea9eafb658942b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5fb170ea4eb8efc6d5ac6889a2501c",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "17345007bba70f0ceca3",
+    "url": "/monsters/static/css/main.988ba200.chunk.css"
+  },
+  {
+    "revision": "cb80a15f71da0f888782",
+    "url": "/monsters/static/js/2.668f8c51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.668f8c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17345007bba70f0ceca3",
+    "url": "/monsters/static/js/main.06f5f31f.chunk.js"
+  },
+  {
+    "revision": "3ad366ebbc5ea22c619f",
+    "url": "/monsters/static/js/runtime-main.735d0d8b.js"
+  }
+]);
